49e2e239a7972bf1be32b779a6a3fc8c59897243
Tetyana Gogulya <cheburashka077@ukr.net>
2024-09-20 00:41:58 +0300

    init_1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,18 +7,16 @@ import friends from '../friends.json';
 import transactions from '../transactions.json';
 
 const App = () => {
-  return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </>
-  );
+  return; // <>
+  //   <Profile
+  //     name={userData.username}
+  //     tag={userData.tag}
+  //     location={userData.location}
+  //     image={userData.avatar}
+  //     stats={userData.stats}
+  //   />
+  //   <FriendList friends={friends} />
+  //   <TransactionHistory items={transactions} />
+  // </>
 };
 export default App;
